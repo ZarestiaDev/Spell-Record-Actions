33823b25507d88bb2a119e82d0f2e454721ebcde
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2022-11-26 00:35:39 +0100

    v2.6 Fix 5E spell action deletion

--- campaign/scripts/power_action.lua
+++ campaign/scripts/power_action.lua
@@ -7,7 +7,7 @@ function onInit()
 	registerMenuItem(Interface.getString("power_menu_actiondelete"), "deletepointer", 4);
 	registerMenuItem(Interface.getString("list_menu_deleteconfirm"), "delete", 4, 3);
 	
-	updateDisplay();
+	self.updateDisplay();
 	
 	local node = getDatabaseNode();
 	
@@ -19,65 +19,5 @@ function onInit()
 
 	local sNode = getDatabaseNode().getPath();
 	DB.addHandler(sNode, "onChildUpdate", onDataChanged);
-	onDataChanged();
-end
-
-function onClose()
-	if super and super.onClose then
-        super.onClose();
-    end
-end
-
-function onMenuSelection(selection, subselection)
-	if super and super.onMenuSelection then
-        super.onMenuSelection();
-    end
-end
-
-function highlight(bState)
-	if super and super.highlight then
-        super.highlight();
-    end
-end
-
-function updateDisplay()
-	if super and super.updateDisplay then
-        super.updateDisplay();
-    end
-end
-
-function updateViews()
-	if super and super.updateViews then
-        super.updateViews();
-    end
-end
-
-function onDataChanged()
-	if super and super.onDataChanged then
-        super.onDataChanged();
-    end
-end
-
-function onCastChanged()
-	if super and super.onCastChanged then
-        super.onCastChanged();
-    end
-end
-
-function onDamageChanged()
-	if super and super.onDamageChanged then
-        super.onDamageChanged();
-    end
-end
-
-function onHealChanged()
-	if super and super.onHealChanged then
-        super.onHealChanged();
-    end
-end
-
-function onEffectChanged()
-	if super and super.onEffectChanged then
-        super.onEffectChanged();
-    end
+	self.onDataChanged();
 end
